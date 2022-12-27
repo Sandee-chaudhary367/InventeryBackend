6490aa712efd5cabe0dfcb6ab2fe1cf243f69ad4
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2022-12-27 15:17:05 +0530

    Pushing Aritcle cODMJE 2887

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 //setup to connect to the database
-mongoose.connect('mongodb://127.0.0.1:27017/Inventory2', {
+let url=    "mongodb+srv://sandeepchaudhary:NewCommer13@cluster0.abp9usm.mongodb.net/Cluster0?retryWrites=true&w=majority"
+// mongoose.connect('mongodb://127.0.0.1:27017/Inventory2', {
+    mongoose.connect(url, {
     useNewUrlParser: true,
 //  useCreateIndex: true,  cause error - Operation `users.insertOne()` buffering timed out after 10000ms
     useUnifiedTopology: true
